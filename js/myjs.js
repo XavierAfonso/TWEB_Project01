74c7e693ecdf1d6237e6e9064acc34eb969bf2fe
Xavier Afonso <xavier.vazafonso@heig-vd.ch>
2018-10-26 18:28:18 +0200

    Create tests with Mocha and Chai

--- js/myjs.js
+++ js/myjs.js
@@ -1,17 +1,37 @@
 
 // https://medium.freecodecamp.org/environment-settings-in-javascript-apps-c5f9744282b6
-// eslint-disable-next-line
-const baseUrl =  (window.location.hostname === 'localhost' || (window.location.hostname === '127.0.0.1'))
+let baseUrl;
+
+try{
+  baseUrl = (window.location.hostname === 'localhost' || (window.location.hostname === '127.0.0.1'))
   ? 'http://localhost:3000'
   : 'https://tweb-project1.herokuapp.com';
+   
+}
+// eslint-disable-next-line
+catch(err) {
+console.log("Window not loaded");
+}
 
 let nodes = [];
 let edges = [];
+
+var nodesPublic = [];
+var edgesPublic = [];
+
 let network = null;
+
+function getNodes() {
+  return nodesPublic;
+}
+
+function getedges() {
+  return edgesPublic;
+}
 
 function loading() {
   $('#mynetwork').empty();
-  $('#mynetwork').append('<img id ="loading" src="gif/loading.gif" alt="Loading">');  
+  $('#mynetwork').append('<img id ="loading" src="gif/loading.gif" alt="Loading">');
 }
 
 // Called when the Visualization API is loaded.
@@ -64,6 +84,88 @@ function draw() {
       .then(res => res.json());
 } */
 
+function createGraph(data) {
+  const arrayIds = [];
+  nodes = [];
+  edges = [];
+  const edgesMemory = [];
+
+  try {
+    let first = true;
+    const nodeHelpColor = {};
+    nodeHelpColor.border = '#FFD700';
+    data.forEach((item) => {
+      // eslint-disable-next-line
+      const root = item.root;
+      // The root
+      if (first) {
+        const rootClean = {};
+        rootClean.id = root.id;
+        // Add to the list
+        arrayIds.push(rootClean.id);
+        // Create the node
+        rootClean.shape = 'circularImage';
+        rootClean.image = root.avatar_url;
+        rootClean.label = root.login;
+        rootClean.html_url = root.html_url;
+        rootClean.color = '#A0FFA0';
+
+        nodes.push(rootClean);
+        first = false;
+      }
+
+      // Contributors
+      // eslint-disable-next-line
+      const contributors = item.contributors;
+      contributors.forEach((contributor) => {
+        const current = {};
+        current.id = contributor.id;
+        // if not already created
+        if (!arrayIds.includes(current.id)) {
+          // Add to the list
+          arrayIds.push(current.id);
+          // create the node
+          current.shape = 'circularImage';
+          current.image = contributor.avatar_url;
+          current.label = contributor.login;
+          current.html_url = contributor.html_url;
+
+          if (Object.prototype.hasOwnProperty.call(contributor, 'predicate') && contributor.predicate === true) {
+            // node is a helper
+            current.color = nodeHelpColor;
+          }
+          nodes.push(current);
+        }
+
+        // Create the edge
+        const edge = {};
+        const edgeColor = {};
+        edgeColor.inherit = false;
+        edge.color = edgeColor;
+        edge.from = root.id;
+        edge.to = contributor.id;
+
+        if (edge.from !== edge.to) {
+          const edgeInvert = `${edge.to}-${edge.from}`;
+          if (!edgesMemory.includes(edgeInvert)) {
+            // Save the pair
+            edgesMemory.push(`${edge.from}-${edge.to}`);
+            edges.push(edge);
+          }
+        }
+
+        nodesPublic = nodes;
+        edgesPublic = edges;
+
+
+      });
+    });
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 function getContributors(username, predicate) {
   return fetch(`${baseUrl}/contributors/${username}/${predicate}`)
     .then((res) => {
@@ -102,91 +204,19 @@ function showErrorMessage(message) {
   setTimeout(function(){ x.className = x.className.replace('show', ''); }, 3000);
 }
 
-function CreateNodes(username, language) {
-  const arrayIds = [];
-  nodes = [];
-  edges = [];
-  const edgesMemory = [];
-
+function initateGraph(username, language) {
   getContributors(username, language).then((data) => {
-    let first = true;
-    const nodeHelpColor = {};
-    nodeHelpColor.border = '#FFD700';
-
-    data.forEach((item) => {
-      // eslint-disable-next-line
-      const root = item.root;
-      // The root
-      if (first) {
-        const rootClean = {};
-        rootClean.id = root.id;
-        // Add to the list
-        arrayIds.push(rootClean.id);
-
-        // Create the node
-        rootClean.shape = 'circularImage';
-        rootClean.image = root.avatar_url;
-        rootClean.label = root.login;
-        rootClean.html_url = root.html_url;
-        rootClean.color = '#A0FFA0';
-
-        nodes.push(rootClean);
-        first = false;
-      }
-
-      // Contributors
-      // eslint-disable-next-line
-      const contributors = item.contributors;
-      contributors.forEach((contributor) => {
-        const current = {};
-        current.id = contributor.id;
-
-        // if not already created
-        if (!arrayIds.includes(current.id)) {
-          // Add to the list
-          arrayIds.push(current.id);
-          // create the node
-          current.shape = 'circularImage';
-          current.image = contributor.avatar_url;
-          current.label = contributor.login;
-          current.html_url = contributor.html_url;
-
-          // eslint-disable-next-line
-          if (contributor.hasOwnProperty('predicate') && contributor.predicate == true) {
-            // node is a helper
-            current.color = nodeHelpColor;
-          }
-          nodes.push(current);
-        }
-
-        // Create the edge
-        const edge = {};
-        const edgeColor = {};
-        edgeColor.inherit = false;
-        edge.color = edgeColor;
-        edge.from = root.id;
-        edge.to = contributor.id;
-
-        if (edge.from !== edge.to) {
-          const edgeInvert = `${edge.to}-${edge.from}`;
-
-          if (!edgesMemory.includes(edgeInvert)) {
-            // Save the pair
-            edgesMemory.push(`${edge.from}-${edge.to}`);
-            edges.push(edge);
-          }
-        }
-      });
-    });
-    // console.log(data);
-    $('#loading').addClass('hidden');
-    draw();
+      if(createGraph(data)){
+        $('#loading').addClass('hidden');
+        draw();
+      }  
   }).catch((err) => {
     console.error(err.message);
     showErrorMessage(err.message);
   });
 }
 
+try{
 $(() => {
   $('#searchButton').click(() => {
     const username = $('#username').val();
@@ -194,8 +224,21 @@ $(() => {
     if (username.length > 0) {
       if (language.length > 0) {
         loading();
-        CreateNodes(username, language);
+        initateGraph(username, language);
       }
     }
   });
 });
+}catch{
+  console.log("Jquery not loaded");
+}
+
+try{
+  module.exports = {
+    getNodes,
+    getedges,
+    createGraph,
+  };
+}catch{
+  console.log("Module not loaded")
+}
